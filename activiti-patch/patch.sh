1a40665c8309e302a37cd9958f640ce71831d7fa
Max Spring <m2spring@springdot.org>
2012-08-24 14:28:46 -0700

    added -B to patch build

--- activiti-patch/patch.sh
+++ activiti-patch/patch.sh
@@ -10,4 +10,4 @@ svn co http://svn.codehaus.org/activiti/projects/designer/trunk/ activiti
 cd activiti
 patch -p0 -i $PRJ/src/main/resources/activiti-designer-jenkow.diff
 
-mvn -f org.activiti.designer.parent/pom.xml clean install
+mvn -B -f org.activiti.designer.parent/pom.xml clean install

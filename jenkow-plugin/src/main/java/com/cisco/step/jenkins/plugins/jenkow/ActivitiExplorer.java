dd70250fcf4d6241e8ee3468472b76f4bcac22ed
Kohsuke Kawaguchi <kk@kohsuke.org>
2012-11-22 07:44:19 -0800

    Control the database connectivity of Activiti Explorer.
    This is because stock activiti explorer war ships with H2 configured to
    make localhost TCP connection.
    
    Eventually I suspect we need to put a custom
    activiti-standlaone-context.xml but that's an exercise for another day.
    For now, I'll just fire up in-memory H2.

--- jenkow-plugin/src/main/java/com/cisco/step/jenkins/plugins/jenkow/ActivitiExplorer.java
+++ jenkow-plugin/src/main/java/com/cisco/step/jenkins/plugins/jenkow/ActivitiExplorer.java
@@ -3,14 +3,14 @@ package com.cisco.step.jenkins.plugins.jenkow;
 import com.cloudbees.vietnam4j.ProxiedWebApplication;
 import hudson.Extension;
 import hudson.model.UnprotectedRootAction;
-import hudson.util.MaskingClassLoader;
 import org.kohsuke.stapler.StaplerRequest;
 import org.kohsuke.stapler.StaplerResponse;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
-import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
 
 /**
  * @author Kohsuke Kawaguchi
@@ -41,11 +41,20 @@ public class ActivitiExplorer implements UnprotectedRootAction {
 
     private synchronized ProxiedWebApplication getProxyWebApplication(StaplerRequest req) throws ServletException {
         if (webApp==null) {
-            webApp = new ProxiedWebApplication(new File("/home/kohsuke/Downloads/activiti-explorer.war"),
-                    req.getContextPath()+'/'+getUrlName());
-            webApp.setParentClassLoader(HttpServletRequest.class.getClassLoader());
-
             try {
+                final ClassLoader ourLoader = getClass().getClassLoader();
+
+                webApp = new ProxiedWebApplication(
+                        ourLoader.getResource("activiti-explorer.war"),
+                        req.getContextPath()+'/'+getUrlName());
+
+                webApp.addClassPath(ourLoader.getResource("activiti-explorer-override.jar"));
+
+                // pass through to the servlet container so that Activiti won't get confused
+                // by what Jenkins loads (e.g., log4j version inconsistency)
+                // but we do expose some selected resources to control its behavior.
+                webApp.setParentClassLoader(HttpServletRequest.class.getClassLoader());
+
                 webApp.start();
             } catch (Exception e) {
                 throw new ServletException(e);

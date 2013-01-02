d447110b301b93eac89e2465d7405ddcfc9f8ea6
Kohsuke Kawaguchi <kk@kohsuke.org>
2013-01-02 11:23:06 -0800

    fixed a test case problem

--- jenkow-plugin/src/test/java/com/cisco/step/jenkins/plugins/jenkow/DiagramTest.java
+++ jenkow-plugin/src/test/java/com/cisco/step/jenkins/plugins/jenkow/DiagramTest.java
@@ -1,8 +1,8 @@
 package com.cisco.step.jenkins.plugins.jenkow;
 
-import hudson.model.FreeStyleBuild;
 import hudson.model.Descriptor;
 import hudson.model.FreeStyleProject;
+import hudson.remoting.Which;
 import hudson.tasks.Builder;
 import hudson.tasks.Shell;
 import hudson.util.DescribableList;
@@ -14,9 +14,10 @@ import javax.imageio.ImageIO;
 
 public class DiagramTest extends JenkowTestCase {
     
-    public void testDiagramGeneration() throws Exception{
+    public void testDiagramGeneration() throws Exception {
         System.out.println("hello world");
-        
+        System.out.println(Which.jarFile(org.apache.xpath.compiler.FunctionTable.class));
+
         JenkowWorkflowRepository repo = JenkowPlugin.getInstance().repo;
         repo.ensureWorkflowDefinition("wf1");
         
@@ -24,17 +25,11 @@ public class DiagramTest extends JenkowTestCase {
         DescribableList<Builder,Descriptor<Builder>> bl = launcher.getBuildersList();
         bl.add(new JenkowBuilder("wf1"));
         bl.add(new Shell("echo wf.done"));
-        FreeStyleBuild build = launcher.scheduleBuild2(0).get();
-        
-        try {
-            // TODO kk? why does this give a valid stream only *after* I manually visit&save the job's configure page?
-            URL url = new URL(new WebClient().getContextPath()+"job/j1/jenkow/graph");
-            System.out.println("url="+url);
-            BufferedImage img = ImageIO.read(url);
-            System.out.println("img="+img);
-        } catch (Exception e) {
-            // eat the exception
-            e.printStackTrace();
-        }
+        configRoundtrip(launcher);  // work around the problem in the core of not registering our builder's project actions
+
+        URL url = new URL(new WebClient().getContextPath()+"job/j1/jenkow/graph");
+        System.out.println("url="+url);
+        BufferedImage img = ImageIO.read(url);
+        System.out.println("img="+img);
     }
 }
